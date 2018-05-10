5ab5b7ae1b5e4e28f2e059409b4fad5a767fc712
Miguel Martinez <miguelitalia@hotmail.com>
2018-05-10 15:52:35 +0000

    added second bar chart with averages of current ratio per industry

--- graphs.js
+++ graphs.js
@@ -79,6 +79,45 @@ function makeGraph(error, securitiesData) {
         .xAxisLabel("Industries")
         .yAxis().ticks(5);
 
+    // sector against current ratio-------------------------
+    
+    let currentGroup = sectorDim.group().reduce(
+        function reduceAdd(p, v) {
+            p.count++;
+            p.total += +v.currentRatio;
+            p.average = p.total / p.count;
+            return p;
+        },
+        function reduceRemove(p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.total = 0;
+                p.average = 0;
+            }
+            else {
+                p.total -= +v.currentRatio;
+                p.average = p.total / p.count;
+            }
+            return p;
+        },
+        function reduceInit() {
+            return { count: 0, total: 0, average: 0 };
+        }
+    );
+
+    dc.barChart("#sectorCurrentSplit")
+        .height(300)
+        .width(1000)
+        .dimension(sectorDim)
+        .group(currentGroup)
+        .valueAccessor(function(p) {
+            return p.value.average;
+        })
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Industries")
+        .yAxis().ticks(5);
+    
 
     dc.renderAll();
 
