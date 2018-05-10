092cd0c781d1ffbac48f51afe69ba9b43e0e44a7
Miguel Martinez <miguelitalia@hotmail.com>
2018-05-10 15:39:49 +0000

    introduced the average on the sectorCashSplit graph

--- graphs.js
+++ graphs.js
@@ -5,54 +5,81 @@ queue()
 function makeGraph(error, securitiesData) {
 
     let ndx = crossfilter(securitiesData);
-// dimensions --------------------------------------------------
+    // dimensions --------------------------------------------------
     let companyNameDim = ndx.dimension(dc.pluck("security"));
-    
+
     let sectorDim = ndx.dimension(dc.pluck("sector"));
-    
+
     let cashDim = ndx.dimension(dc.pluck("cashRatio"));
-    
+
     let currentDim = ndx.dimension(dc.pluck("currentRatio"));
-    
+
     let earningsDim = ndx.dimension(dc.pluck("earningsBeforeInterestAndTax"));
-    
+
     let profitDim = ndx.dimension(dc.pluck("grossProfit"));
-    
+
     let flowDim = ndx.dimension(dc.pluck("netCashFlow"));
-    
+
     let quickDim = ndx.dimension(dc.pluck("quickRatio"));
-    
-// graphs -----------------------------------------------------------    
+
+    // graphs -----------------------------------------------------------   
+    // Sectors graph-------------------------------------------
     let industryGroup = sectorDim.group();
-    
+
     dc.pieChart("#industrySplit")
         .height(500)
         .width(400)
         .dimension(sectorDim)
         .group(industryGroup);
-    
-    
+
+    // companies graph-------------------------------------------
     let securityGroup = companyNameDim.group();
-    
-    dc.pieChart("#sectorSplit")
+
+    dc.pieChart("#companySplit")
         .height(500)
-        .width(400)
+        .width(500)
         .dimension(companyNameDim)
         .group(securityGroup);
-    
-    let cashGroup = sectorDim.group().reduceSum(dc.pluck("cashRatio"));
-    
+
+    //chart sector against cash ratio-------------------------
+    let cashGroup = sectorDim.group().reduce(
+        function reduceAdd(p, v) {
+            p.count++;
+            p.total += +v.cashRatio;
+            p.average = p.total / p.count;
+            return p;
+        },
+        function reduceRemove(p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.total = 0;
+                p.average = 0;
+            }
+            else {
+                p.total -= +v.cashRatio;
+                p.average = p.total / p.count;
+            }
+            return p;
+        },
+        function reduceInit() {
+            return { count: 0, total: 0, average: 0 };
+        }
+    );
+
     dc.barChart("#sectorCashSplit")
         .height(300)
-        .width(800)
+        .width(1000)
         .dimension(sectorDim)
         .group(cashGroup)
+        .valueAccessor(function(p) {
+            return p.value.average;
+        })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Industries")
         .yAxis().ticks(5);
-    
-    
+
+
     dc.renderAll();
-    
+
 }
